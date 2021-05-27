15949ee6361fd3f66b2e5399de8888224fa88786
gonzalodutto <gonzalodutto@gmail.com>
2021-05-27 19:48:33 -0300

    #3

--- main.js
+++ main.js
@@ -1,20 +1,27 @@
 "use strict";
 
+// Se contiene en una constante la direccion web del archivo json:
 const endpoint =
   "https://spreadsheets.google.com/feeds/cells/1URGwdSagiUivMfv4SkdBp9zb5HiVjTHBQzTicKiwSiQ/1/public/full?alt=json";
 
+// Se crea una array vacia (y se contiene en constantes) para los datos (a filtrar y extraer del archivo json remoto) de los productos:
+//
 const productos = [];
 const productosDetalle = [];
 
+// Se solicita informacion del origen remoto json:
 fetch(endpoint)
   .then((res) => res.json())
+  // Se guarda en la array contenida en la constante "productos" los datos necesario (se extrae la informacion correcpondiente a cada celda del documento Google Sheet):
   .then((data) => productos.push(...data.feed.entry));
-// .then((data) => (console.log(data.feed.entry)));
 
+// En la siguiente funcion se crea el proceso por el cual se hace el filtrado de la palabra buscada:
 function findMatches(wordToMatch, productos) {
   return productos.filter((producto) => {
     const regex = new RegExp(wordToMatch, "gi");
+    // Con el "if" siguiente se condiciona el filtrado solo a las celdas pertenecientes a la columna 2:
     if (producto.gs$cell.col === "2") {
+      // La siguiente linea de codigo regresa todos los articulos que coinciden con la el criterio de la "Expresion regular" creada:
       return producto.content.$t.match(regex);
     }
   });
@@ -26,7 +33,7 @@ function displayMatches() {
     .map((producto) => {
       return `
         <li>
-            <span class="name">${producto.content.$t}</span>
+            <span class="name">${producto.content.$t} ($Precio)</span>
         </li>
       `;
     })
