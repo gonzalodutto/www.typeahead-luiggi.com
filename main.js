cec9eae6a760f86163ac14d0ac41ce756f9ad2e4
gonzalodutto <gonzalodutto@gmail.com>
2021-06-08 12:18:28 -0300

    #8 Incorporacion de centavos

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 "use strict";
 
 const endpoint =
-  "https://spreadsheets.google.com/feeds/cells/1URGwdSagiUivMfv4SkdBp9zb5HiVjTHBQzTicKiwSiQ/1/public/full?alt=json";
+  "https://spreadsheets.google.com/feeds/cells/1IHYqDTik3RRIDZH2N5v1Kq1GcIvpVvIjGrBFk9Eg9P8/15/public/full?alt=json"; // Json Hoja de calculos general ferrreteria.
+// const endpoint =
+//   "https://spreadsheets.google.com/feeds/cells/1URGwdSagiUivMfv4SkdBp9zb5HiVjTHBQzTicKiwSiQ/1/public/full?alt=json"; // Json hoja de calcula precios solos.
 
 const productos = [];
-const productosDetalle = [];
 
 fetch(endpoint)
   .then((res) => res.json())
@@ -25,8 +26,13 @@ function displayMatches() {
         <li>
             <span class="name">
               <b>${producto.content.$t.split(" > ")[0]}</b> 
-               - ${producto.content.$t.split(" > ")[1]} -
-              <b> $ ${producto.content.$t.split(" > ")[2]}</b>
+                - ${producto.content.$t.split(" > ")[1]} -
+              <b> $ ${
+                producto.content.$t.split(" > ")[2].split(",")[0]
+              },${producto.content.$t
+        .split(" > ")[2]
+        .split(",")[1]
+        .slice(0, 2)}</b>
             </span>
         </li>
       `;
