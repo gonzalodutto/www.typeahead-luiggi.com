bf61b9303583dac2cdbe1f00ef4ad9699d7b99f2
gonzalodutto <gonzalodutto@gmail.com>
2021-06-17 22:25:23 -0300

    #11 Agregado de invoice section

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ const endpoint =
 
 const productos = [];
 
+/////////////////////////////////////////////////
+// Elements
+const labelDate = document.querySelector(".date");
+
 fetch(endpoint)
   .then((res) => res.json())
   .then((data) => productos.push(...data.feed.entry));
@@ -20,17 +24,14 @@ function displayMatches() {
   const matchArray = findMatches(this.value, productos);
   const html = matchArray
     .map((producto) => {
+      const codigo = producto.content.$t.split(" > ")[0].padEnd(7, " ");
+      const descripcion = producto.content.$t.split(" > ")[1];
+      const precio = Intl.NumberFormat("de-DE").format(
+        Number(producto.content.$t.split(" > ")[2].replace(",", ".")).toFixed(2)
+      );
       return `
         <li>
-            <span class="name">
-              <b>${producto.content.$t.split(" > ")[0].padEnd(7, " ")}</b> 
-                - ${producto.content.$t.split(" > ")[1]} -
-              <b> $ ${Intl.NumberFormat("de-DE").format(
-                Number(
-                  producto.content.$t.split(" > ")[2].replace(",", ".")
-                ).toFixed(2)
-              )}</b>
-            </span>
+            <span class="name"><b>${codigo}</b> - ${descripcion} - <b> $ ${precio}</b></span>
         </li>
       `;
     })
@@ -43,3 +44,12 @@ const suggestions = document.querySelector(".suggestions");
 
 searchInput.addEventListener("change", displayMatches);
 searchInput.addEventListener("keyup", displayMatches);
+
+// Date format
+const now = new Date();
+const day = `${now.getDate()}`.padStart(2, 0);
+const month = `${now.getMonth() + 1}`.padStart(2, 0);
+const year = now.getFullYear();
+const hour = now.getHours();
+const min = now.getMinutes();
+labelDate.textContent = `${day}/${month}/${year}`;
